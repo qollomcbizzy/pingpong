56c179239ee1fff339bceb9c9b577362326c978a
collo <qollomcbizzy64@gmail.com>
2017-05-05 16:31:01 +0300

    added the backend logic

--- js/script.js
+++ js/script.js
@@ -1,17 +1,41 @@
 $(document).ready(function(){
   //the backend logic
-  var change=function(x){
-
-  };
+  var countUp = function(countTo){
+  var result = [];
+  for(var i = 1; i <= countTo; i++){
+     var index = result.indexOf(i);
+     //checks if the number is divisible by both 3 and five first
+       if((i % 3 === 0) && (i % 5 === 0)) {
+        result.splice(index, 0);
+        result.push('Pingpong');
+        }
+      // checks if the number is divisible by three if not by both five and itself
+       else  if(i % 3=== 0) {
+        result.splice(index, 0);
+        result.push('ping');
+        }
+     //checks if the number is divisible by five
+       else if(i % 5 === 0) {
+        result.splice(index, 0);
+        result.push('pong');
+        }
+       else{
+        result.push(i);
+        }
+       }
+       return result;
+       };
+//User Interface Logic
   $("form#pingpong").submit(function(event){
     event.preventDefault();
     var number=String(parseInt($("input#number").val()));
     if(number.match(/[0123456789]/)){
-        $("#output").text(number);
+    var result=countUp(number);
+      $("#output").text(result);
     }
     else {
       var not="That's not a number";
       $("#output").text(not);
     }
-  })
+  });
 });
